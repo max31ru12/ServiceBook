9698a7d2e89918f71e7e56173af609d692dd3573
max31ru12 <maxevg72@gmail.com>
2025-02-02 22:19:34 +0300

    new: initialized cars domain

---
+++ app/domains/cars/models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.core.setup_db import Base
+
+
+class Brand(Base):
+    __tablename__ = "brand"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column()
+
+
+class Car(Base):
+    __tablename__ = "car"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    model: Mapped[str] = mapped_column()
+    year: Mapped[int] = mapped_column()
+
+    brand_id: Mapped[int] = mapped_column(ForeignKey("brand.id"))
+    brand: Mapped["Brand"] = relationship(back_populates="cars")
