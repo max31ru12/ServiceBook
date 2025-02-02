9999ff849048f74a0e3711a58ffc3e37acb9187f
max31ru12 <maxevg72@gmail.com>
2025-02-02 22:20:32 +0300

    new: initialized cars domain

--- alembic/env.py
+++ alembic/env.py
@@ -8,6 +8,8 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from alembic import context
 from app.core.config import DB_URL
 from app.core.setup_db import Base
+from app.domains.cars.models import Brand, Car  # noqa necessary model import
+from app.domains.users.models import User  # noqa necessary model import
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
