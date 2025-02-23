6b45ee99114066193e0bf2ee9392b33691fb1c2e
max31ru12 <maxevg72@gmail.com>
2025-02-24 00:16:26 +0300

    new: Исправил ошибку при использовании одного класса в нескольких эндпоинтах

--- app/core/responses.py
+++ app/core/responses.py
@@ -5,12 +5,18 @@ from pydantic import BaseModel
 
 
 class Responses:
+    __original_attrs = {}
+
     @classmethod
     def get_responses(cls) -> dict[int | str, dict[str, Any]] | None:
         responses_dict = {}
         for attr in dir(cls):
-            if not attr.startswith("__") and not callable(getattr(cls, attr)):
-                status_code, detail = getattr(cls, attr)
+            if not attr.startswith("__") and not callable(getattr(cls, attr)) and attr != "attrs":
+                try:
+                    status_code, detail = getattr(cls, attr)
+                    cls.__original_attrs[attr] = status_code, detail
+                except Exception:  # noqa No matter what exception catches
+                    status_code, detail = cls.__original_attrs[attr]
                 if status_code not in responses_dict.keys():
                     responses_dict[status_code] = {
                         "description": attr.replace("_", " "),
